98c0b88431140a56596affc4b7fdf22b85e08039
Jam1ll <jamilguzman202@gmail.com>
2025-11-05 18:59:09 -0400

    product events and product class added

---
+++ BabelBooks.Core.Domain/Events/ProductEvents.cs
@@ -0,0 +1,13 @@
+﻿namespace BabelBooks.Core.Domain.Events
+{
+    public record ProductCreatedEvent(
+        Guid ProductId,
+        string ProductName,
+        decimal ProductPrice
+        );
+
+    public record ProductPriceUpdatedEvent(
+        Guid ProductId,
+        decimal ProductNewPrice
+        );
+}
--- BabelBooks.Core.Domain/Shared/AggregateBase.cs
+++ BabelBooks.Core.Domain/Shared/AggregateBase.cs
@@ -4,27 +4,20 @@
     {
         public Guid Id { get; protected set; }
 
-        //eventos que han ocurrido pero no han sido guardados
-        private readonly List<object> _uncommitedEvents = [];
+        private readonly List<object> _uncommitedEvents = []; //eventos que han ocurrido pero no han sido guardados
 
-        //metodo para obtener los eventos no guardados
-        public object[] GetUncommitedEvents() => [.. _uncommitedEvents]; // -> _uncommitedEvents.ToArray();
+        public object[] GetUncommitedEvents() => [.. _uncommitedEvents]; //metodo para obtener los eventos no guardados
 
-        //metodo para limpiar la lista de eventos luego de guardar
-        public void ClearUncommitedEvents()=> _uncommitedEvents.Clear();
+        public void ClearUncommitedEvents()=> _uncommitedEvents.Clear(); //metodo para limpiar la lista de eventos luego de guardar
 
-        //metodo para aplicar un evento al estado del agregado actual
-        protected void ApplyEvent(object @event)
+        protected void ApplyEvent(object @event) //metodo para aplicar un evento al estado del agregado actual
         {
-            //llamar al metodo ApplyEvent especifico para el tipo de evento mediante reflection
-            var method = GetType().GetMethod("ApplyEvent", [@event.GetType()])
-                //si no se encuentra el metodo...
-                ?? throw new InvalidOperationException($"No se encontro el metodo ApplyEvent para el evento de tipo {@event.GetType().Name}");
+            var method = GetType().GetMethod("ApplyEvent", [@event.GetType()]) //llamar al metodo ApplyEvent especifico para el tipo de evento mediante reflection
+                ?? throw new InvalidOperationException($"No se encontro el metodo ApplyEvent para el evento de tipo {@event.GetType().Name}"); //si no se encuentra el metodo...
             method.Invoke(this, [@event]);
         }
         
-        //metodo para que las clases derivadas registren un nuevo evento
-        public void RaiseEvent(object @event)
+        public void RaiseEvent(object @event) //metodo para que las clases derivadas registren un nuevo evento
         {
             ApplyEvent(@event); //aplicar evento al estado actual del agregado
             _uncommitedEvents.Add(@event); //agregar evento a la lista de eventos no guardados
