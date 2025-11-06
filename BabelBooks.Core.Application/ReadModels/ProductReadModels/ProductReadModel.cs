ccc60928cc9c5d49d99a41b40dc46d080d9dc811
Jam1ll <jamilguzman202@gmail.com>
2025-11-06 13:54:33 -0400

    Product read model added

---
+++ BabelBooks.Core.Application/ReadModels/ProductReadModels/ProductReadModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BabelBooks.Core.Application.ReadModels.ProductReadModels
+{
+    public class ProductReadModel
+    {
+
+        public Guid Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public decimal ProductPrice { get; set; }
+    }
+}
+//Esta es la clase 'denormalizada' que se utiliza para las consultas de productos
+//Marten la guardará como un documento JSON y la API la utilizará para devolver datos al cliente
