779bb891f8f2ec82756ccdea4860179c94fb456f
Jam1ll <jamilguzman202@gmail.com>
2025-11-06 12:10:29 -0400

    business logic on Product aggregate done. Includes factory method in order to create event and Update method to update product's price. Finally, mutators to apply changes when an event is triggered.

---
+++ BabelBooks.Core.Domain/Aggregates/Product.cs
@@ -0,0 +1,63 @@
+﻿using BabelBooks.Core.Domain.Events;
+using BabelBooks.Core.Domain.Shared;
+
+namespace BabelBooks.Core.Domain.Aggregates
+{
+    public class Product : AggregateBase
+    {
+        public string ProductName { get; private set; } = string.Empty;
+        public decimal ProductPrice { get; private set; }
+        public int ProductVersion { get; private set; }
+
+        public Product() { } //constructor vacio para la hidratacion del agregado via Marten
+
+        //
+        // logica de negocio
+        //
+
+        //factory method para crear un nuevo producto
+        public static Product Create(string productName, decimal productPrice)
+        {
+            //verificacion de reglas de negocio
+            if (string.IsNullOrWhiteSpace(productName))
+                throw new ArgumentException($"Product name can't be empty");
+            if (productPrice < 0)
+                throw new ArgumentException($"Product price can't be negative");
+
+            //crear instancia
+            var product = new Product();
+
+            //generar evento. Esto reemplaza el tipico this.ProductId = Guid.NewGuid();, etc
+            product.RaiseEvent(new ProductCreatedEvent(Guid.NewGuid(), productName, productPrice));
+
+            return product;
+        }
+
+        public void UpdateProductPrice(decimal productNewPrice)
+        {
+            if (productNewPrice <= 0)
+                throw new ArgumentException("Price should be positive.");
+            if (productNewPrice == ProductPrice)
+                return; // no changes == no event
+
+            //generar evento
+            RaiseEvent(new ProductPriceUpdatedEvent(Id, productNewPrice));
+        }
+
+        //metodos ApplyEvent (mutadores) para actualizar el estado del agregado en respuesta a eventos
+
+        public void ApplyEvent(ProductCreatedEvent @event)
+        {
+            Id = @event.ProductId;
+            ProductName = @event.ProductName;
+            ProductPrice = @event.ProductPrice;
+            ProductVersion++;
+        }
+
+        public void ApplyEvent(ProductPriceUpdatedEvent @event)
+        {
+            ProductPrice = @event.ProductNewPrice;
+            ProductVersion++;
+        }
+    }
+}
